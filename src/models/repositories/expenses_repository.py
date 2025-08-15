b85d7294294498ad55bc235a4f70ff4c56ce29c9
joaopaulogui <joaopaulogmormota@gmail.com>
2025-08-15 17:36:17 -0300

    repository: add expense repositories

---
+++ src/models/repositories/expenses_repository.py
@@ -0,0 +1,59 @@
+import datetime
+from src.models.settings.connection import DBConnectionHandler
+from src.models.entities.expenses import Expense as ExpenseEntity
+
+class ExpensesRepository:
+
+    @classmethod
+    def insert_expense(cls, description: str, type: str, date: str, cost: int) -> ExpenseEntity:
+        with DBConnectionHandler() as database:
+            try:
+                new_registry = ExpenseEntity(description, type, date, cost)
+                database.session.add(new_registry)
+                database.session.commit()
+                return new_registry
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+            
+    @classmethod
+    def select_expense_by_type(cls, type: str) -> any:
+        with DBConnectionHandler() as database:
+            try:
+                expenses = (
+                    database.session
+                        .query(ExpenseEntity)
+                        .filter(ExpenseEntity.type == type)
+                        .all()
+                )
+                return expenses
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+            
+    @classmethod
+    def select_expense_by_date(cls, date: str) -> any:
+        with DBConnectionHandler() as database:
+            try:
+                y, m, d = date.split("-")
+
+                expenses = (
+                    database.session
+                        .query(ExpenseEntity)
+                        .filter(ExpenseEntity.date == datetime.date(int(y), int(m), int(d)))
+                        .all()
+                )
+                return expenses
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+
+    @classmethod
+    def delete_expense(cls, id: int) -> None:
+         with DBConnectionHandler() as database:
+            try:
+                database.session.query(ExpenseEntity).filter(ExpenseEntity.id == id).delete()
+                database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
---
+++ src/models/repositories/expenses_repository_test.py
@@ -0,0 +1,18 @@
+from .expenses_repository import ExpensesRepository
+
+def test_repository_expense():
+    mocked_description = "description"
+    mocked_type = "type"
+    mocked_cost = 0
+    mocked_date = "2000-01-01"
+
+    expenses_repository = ExpensesRepository()
+
+    expenses_repository.insert_expense(mocked_description, mocked_type, mocked_date, mocked_cost)    
+
+    response = expenses_repository.select_expense_by_type("type")
+    print()
+    print(response)
+
+    for expense in response:
+        expenses_repository.delete_expense(expense.id)
