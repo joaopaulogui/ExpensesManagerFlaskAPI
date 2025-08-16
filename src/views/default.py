a2ca6d05ad21e1ca9722822d8152c79669e62e48
joaopaulogui <joaopaulogmormota@gmail.com>
2025-08-16 17:48:39 -0300

    view: solved bug in date

--- src/views/default.py
+++ src/views/default.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from src.controllers.expense_register_controller import RegisterExpenseForm
 from src.controllers.expense_register_controller import ExpenseRegisterController
 from src.controllers.expense_lister_controller import ExpenseListerController
@@ -13,10 +13,23 @@ app.config['SECRET_KEY'] = 'secret_key'
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    selected_date = request.form.get("date") or datetime.today().strftime("%d/%m/%Y")
-
+    selected_date = request.args.get("date") or datetime.today().strftime("%d/%m/%Y")
     query_date = datetime.strptime(selected_date, "%d/%m/%Y").strftime("%Y-%m-%d")
 
+    form = RegisterExpenseForm()
+    if request.method == "POST":
+        if "date" in request.form:
+            selected_date = request.form.get("date")
+        elif "cost" in request.form and form.validate_on_submit():
+            expense_register_controller = ExpenseRegisterController()
+            expense_register_controller.insert_expense(
+                form.description.data,
+                form.type.data,
+                query_date,
+                form.cost.data
+            )
+        return redirect(url_for("index", date=selected_date))
+    
     expense_lister_controller = ExpenseListerController()
     lister_response = expense_lister_controller.select_expense_by_date(query_date)
 
@@ -34,18 +47,6 @@ def index():
     else:
         divided_total = {}
         total = 0
-
-    form = RegisterExpenseForm()
-    if request.method == "POST" and "submit_expense" in request.form:
-        if form.validate_on_submit():
-            expense_register_controller = ExpenseRegisterController()
-            expense_register_controller.insert_expense(
-                form.description.data,
-                form.type.data,
-                selected_date,
-                form.cost.data
-            )
-            form = RegisterExpenseForm()
 
     return render_template(
         'index.html',
